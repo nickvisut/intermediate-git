19e2c373f6ceb6ab35fde344a1e89381e35d2d32
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2025-12-18 17:32:45 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+import test from './sum.test.js';
+
+test();
---
+++ sum.test.js
@@ -0,0 +1,10 @@
+import sum from './sum.js';
+
+function test() {
+  const a = 3;
+  const b = 5;
+  const result = sum(a, b);
+  console.log(result)
+}
+
+export default test;
