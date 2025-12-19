c04bd382ae76aa76c092388f7d0b65c604c8820f
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2025-12-18 17:41:00 -0800

    store rrdult in intermediate varasjte

--- sum.js
+++ sum.js
@@ -3,6 +3,7 @@ function sum(a, b) {
   console.log(`a: ${a}`);
   console.log(`b: ${b}`);
 
+  // use intermediate variable to store the result
   const result = a * b;
   console.log(`result: ${result}`;
   return result;
